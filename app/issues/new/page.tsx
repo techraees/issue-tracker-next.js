b2c15f563fa75ea4405daa8e69967e391bdda689
techraees <techraees786@gmail.com>
2023-11-24 01:58:32 +0500

    Disable SSR for loading markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button, Callout, Text, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
+import dynamic from "next/dynamic";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
@@ -11,6 +11,8 @@ import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {ssr:false});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -25,17 +27,17 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema),
   });
   const [error, setError] = useState("");
-  const  [isSubmitting,setIsSubmitting] = useState(false)
-const onSubmit = handleSubmit(async (data) => {
-  try {
-    setIsSubmitting(true)
-    await axios.post("/api/issues", data);
-    router.push("/issues");
-  } catch (error) {
-    setIsSubmitting(false)
-    setError("An unexpected error occur");
-  }
-})
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setIsSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setIsSubmitting(false);
+      setError("An unexpected error occur");
+    }
+  });
   return (
     <div className="max-w-xl">
       {error && (
@@ -43,17 +45,14 @@ const onSubmit = handleSubmit(async (data) => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        className="max-w-xl space-y-3"
-        onSubmit={onSubmit}
-      >
+      <form className="max-w-xl space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input
             placeholder="Search the docs…"
             {...register("title")}
           />
         </TextField.Root>
-         <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
@@ -61,9 +60,11 @@ const onSubmit = handleSubmit(async (data) => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-          <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting &&<Spinner />}</Button>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
