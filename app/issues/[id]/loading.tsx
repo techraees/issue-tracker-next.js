ed01602f758acafb6357278a09e8e4e1cf29d00c
techraees <techraees786@gmail.com>
2023-11-24 02:22:07 +0500

    Add the edit button.

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -7,7 +7,6 @@ const LoadingIssueDetailPage = () => {
       <Skeleton  />
       <Flex gap="3" my="2">
         <Skeleton width="5rem" />
-
         <Skeleton width="8rem" />
       </Flex>
       <Card className="prose" mt="4">
