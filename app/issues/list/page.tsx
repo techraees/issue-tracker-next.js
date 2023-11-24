83034ab96ee12015fd9b66e4fcc6c301c47c2b00
techraees <techraees786@gmail.com>
2023-11-25 03:52:22 +0500

    Fix filtering bugs.

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -44,7 +44,7 @@ const IssuePage = async ({
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
-              <Table.ColumnHeaderCell key={column.value}>
+              <Table.ColumnHeaderCell key={column.value} className={column.className}>
                 <NextLink
                   href={{
                     query: { ...searchParams, orderBy: column.value },
