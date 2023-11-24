66e10e8b4436dfabf472d8f90fe93a5919380705
techraees <techraees786@gmail.com>
2023-11-24 14:37:56 +0500

    Build an API for deleting issue.

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -26,5 +26,25 @@ export async function PATCH(
     },
   });
 
-  return NextResponse.json(updatedIssue)
+  return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  await prisma.issue.delete({
+    where: { id: parseInt(params.id) },
+  });
+
+  return NextResponse.json({});
 }
