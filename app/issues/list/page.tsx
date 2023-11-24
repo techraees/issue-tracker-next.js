25ca6e7da4e2bf0f01c9d6d8410e465299d04123
techraees <techraees786@gmail.com>
2023-11-25 04:40:50 +0500

    Paginate issues.

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -5,11 +5,16 @@ import NextLink from "next/link";
 import IssueActions from "./IssueActions";
 import { Issue, Status } from "@prisma/client";
 import { ArrowUpIcon } from "@radix-ui/react-icons";
+import Pagination from "@/app/components/Pagination";
 
 const IssuePage = async ({
   searchParams,
 }: {
-  searchParams: { status: Status; orderBy: keyof Issue };
+  searchParams: {
+    status: Status;
+    orderBy: keyof Issue;
+    page: string;
+  };
 }) => {
   const columns: {
     label: string;
@@ -26,16 +31,25 @@ const IssuePage = async ({
     ? searchParams.status
     : undefined;
 
-  const orderBy = columns.map(column  =>column.value).includes(searchParams.orderBy) 
+  const where = { status };
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: "asc" }
     : undefined;
 
+  const page = parseInt(searchParams.page) || 1;
+  const pageSize = 10;
+
   const issues = await prisma.issue.findMany({
-    where: {
-      status,
-    },
-    orderBy
+    where,
+    orderBy,
+    skip: (page - 1) * pageSize,
+    take: pageSize,
   });
+
+  const issueCount = await prisma.issue.count({ where });
 
   return (
     <div>
@@ -44,7 +58,10 @@ const IssuePage = async ({
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
-              <Table.ColumnHeaderCell key={column.value} className={column.className}>
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
                 <NextLink
                   href={{
                     query: { ...searchParams, orderBy: column.value },
@@ -78,6 +95,11 @@ const IssuePage = async ({
           ))}
         </Table.Body>
       </Table.Root>
+      <Pagination
+        pageSize={pageSize}
+        currentPage={page}
+        itemCount={issueCount}
+      />
     </div>
   );
 };
