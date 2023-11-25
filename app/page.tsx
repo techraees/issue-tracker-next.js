95d5d3d0efb80945ab6feb917f68322a5544b82c
techraees <techraees786@gmail.com>
2023-11-25 05:19:12 +0500

    Build the latest issue component with bug.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
+import LatestIssues from "./LatestIssues";
 import Pagination from "./components/Pagination";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
+export default function Home() {
   return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={parseInt(searchParams.page)}
-    />
+    <LatestIssues />
   );
 }
